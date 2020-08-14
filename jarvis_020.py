04740bf249d9f41174f4dcd2e1304653cbbed00b
Harsh Vishwakarma <harshprogrammer782@gmail.com>
2020-08-14 22:07:33 +0530

    Version 2 is Ready with Serach on Youtube, Google, Plays Specific music and Check Internet connection

--- jarvis_020.py
+++ jarvis_020.py
@@ -7,7 +7,8 @@ Desciption : This is version 0.1.0 and it can Perform some basic tasks like
              says command before every task,
              search in wikipedia,
              open youtube, google, stackoverflow, hackerrank, facebook, instagram
-             plays music,
+             plays song which was asked,
+             plays random music,
              tell about time,
              open sublime, visual code studio, 
              close itself on command
@@ -96,45 +97,138 @@ def listen():
 # Tasks to be Performed by Jarvis
 def dotask(query):
 
-     if 'wikipedia' in query:
-         query = query.replace('wikipedia', '')
-         result = wikipedia.summary(query, sentences=2)
-         tell("According to Wikipedia....")
-         tell(result)
-     elif 'open youtube' in query :
-         webbrowser.open("youtube.com")
-     elif 'open google' in query:
-         webbrowser.open("google.com")
-     elif 'open facebook' in query:
-         webbrowser.open("facebook.com")
-     elif 'open instagram' in query:
-         webbrowser.open("instagram.com")
-     elif 'open stackoverflow' in query:
-         webbrowser.open("stackoverflow.com")
-     elif 'open hacker rank' in query:
-         webbrowser.open("hackerrank.com")
-     elif 'open code' in query:
-         os.startfile(r'C:\Users\Lenovo\AppData\Local\Programs\Microsoft VS Code\Code.exe')
-     elif 'open sublime' in query:
-         os.startfile(r"C:\Program Files\Sublime Text 3\sublime_text.exe")
-     elif 'open hacker rank' in query:
-         webbrowser.open("hackerrank.com")
-     elif 'open hacker rank' in query:
-         webbrowser.open("hackerrank.com")
-     elif 'who are you' in query:
+    if 'wikipedia' in query:    
+        query = query.replace('wikipedia', '')
+        result = wikipedia.summary(query, sentences=2)
+        tell("According to Wikipedia....")
+        tell(result)
+    elif 'search' in query:
+        query = query.replace('search for',"")
+        query = query.replace('search', "")
+        if 'youtube' in query:
+            query = query.replace('in youtube', "")
+            query = query.replace('on youtube', "")
+            query = query.replace('youtube', "")
+            tell("Searching on YouTube...")
+            webbrowser.open(f"https://www.youtube.com/search?q={query}")
+        else:
+                query = query.replace('in google',"")
+                query = query.replace('on google',"")
+                query = query.replace('google',"")
+                tell("Searching on Google...")
+                webbrowser.open(f"https://www.google.com/search?q={query}")
+    elif 'open' in query:
+        query = query.replace('open','')
+        if 'youtube' in query:
+            tell("Opening Youtube...")
+            webbrowser.open('youtube.com')
+        elif 'google' in query:
+            tell("Opening Google")
+            webbrowser.open("google.com")
+        elif 'facebook' in query:
+            tell("Opening Facebook")
+            webbrowser.open("facebook.com")
+        elif 'instagram' in query:
+            tell("Opening Instagram")
+            webbrowser.open("instagram.com")
+        elif 'stackoverflow' in query or 'stack overflow' in query:
+            tell("Opening Stack Overflow")
+            webbrowser.open("stackoverflow.com")
+        elif 'hacker rank' in query or 'hackerrank' in query:
+            tell("Opening Hacker Rank")
+            webbrowser.open("hackerrank.com")
+        elif 'code' in query:
+            tell("Opening Visual Code Studio")
+            os.startfile(r'C:\Users\Lenovo\AppData\Local\Programs\Microsoft VS Code\Code.exe')
+        elif 'sublime' in query:
+            tell("Opening Sublime Text 3")
+            os.startfile(r"C:\Program Files\Sublime Text 3\sublime_text.exe")
+        elif 'blog' in query:
+            tell("Opening Your Blog Website")
+            webbrowser.open("blog.harshvishwakarma.xyz")
+        elif 'website' in query:
+            tell("Opening Your Website")
+            webbrowser.open("harshvishwakarma.xyz")
+        else:
+            tell(f"Can't Open {query}")
+    elif 'play music' in query or 'play song' in query:
+        query = query.replace('play music', '')
+        query = query.replace('play song', '')
+        query = query.strip()
+        music_dir = r"F:\song listen"
+        songs = os.listdir(music_dir)
+        # print(f"Query :{query}")
+        x=0
+        if query=="":
+            randmusic = songs[random.randint(0, len(songs))]
+            tell(f"PLaying {randmusic}")
+            os.startfile(os.path.join(music_dir, randmusic))
+            x=1
+        else:
+            for i in songs:
+                # print(f"Song : {i}")                
+                if query in i.lower():
+                    tell(f"Playing Song {query}")
+                    os.startfile(os.path.join(music_dir, i))
+                    x=1
+                    break
+        
+        if x==0:
+            tell(f"No Song name {query} in your Directory")
+    elif 'who are you' in query:
          tell("I am Jarvis")
          tell("Virtual Assistant of Harsh Vishwakarma")
          tell("I am on Version 0.2.0")
-         
-     elif 'play music' in query:
-         music_dir = r"F:\song listen"
-         songs = os.listdir(music_dir)
-         os.startfile(os.path.join(music_dir, songs[random.randint(0,len(songs))]))
-     elif 'time' in query:
+
+
+    # For telling the time
+    elif 'time' in query:
          time = datetime.datetime.now().strftime("%H:%M:%S")
          tell(f"Time is {time}")
-     else:
-         isclose(query)
+    else:
+         isclose(query)      
+
+
+
+    #  if 'wikipedia' in query:
+    #      query = query.replace('wikipedia', '')
+    #      result = wikipedia.summary(query, sentences=2)
+    #      tell("According to Wikipedia....")
+    #      tell(result)
+    #  elif 'open youtube' in query :
+    #      webbrowser.open("youtube.com")
+    #  elif 'open google' in query:
+    #      webbrowser.open("google.com")
+    #  elif 'open facebook' in query:
+    #      webbrowser.open("facebook.com")
+    #  elif 'open instagram' in query:
+    #      webbrowser.open("instagram.com")
+    #  elif 'open stackoverflow' in query:
+    #      webbrowser.open("stackoverflow.com")
+    #  elif 'open hacker rank' in query:
+    #      webbrowser.open("hackerrank.com")
+    #  elif 'open code' in query:
+    #      os.startfile(r'C:\Users\Lenovo\AppData\Local\Programs\Microsoft VS Code\Code.exe')
+    #  elif 'open sublime' in query:
+    #      os.startfile(r"C:\Program Files\Sublime Text 3\sublime_text.exe")
+    #  elif 'open hacker rank' in query:
+    #      webbrowser.open("hackerrank.com")
+    #  elif 'open hacker rank' in query:
+    #      webbrowser.open("hackerrank.com")
+    #  elif 'who are you' in query:
+    #      tell("I am Jarvis")
+    #      tell("Virtual Assistant of Harsh Vishwakarma")
+    #      tell("I am on Version 0.2.0")
+         
+    #  elif 'play music' in query:
+    #      music_dir = r"F:\song listen"
+    #      songs = os.listdir(music_dir)
+    #      os.startfile(os.path.join(music_dir, songs[random.randint(0,len(songs))]))
+    #  elif 'time' in query:
+    #      time = datetime.datetime.now().strftime("%H:%M:%S")
+    #      tell(f"Time is {time}")
+    #  else:
+    #      isclose(query)
 
     
 
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+    tell(f"Playing Song {query}")
+                    os.startfile(os.path.join(music_dir, i))
+                    x=1
+                    break
